3f9de26a95bb811b258a85c131941d719ec357f5
Kjell6 <143009706+Kjell6@users.noreply.github.com>
2024-05-28 12:17:03 +0200

    Übung 3 abgeschlossen

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -1,7 +1,6 @@
+import java.awt.*;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
-
 import static java.awt.Color.*;
 
 public class GameLogic {
@@ -16,7 +15,7 @@ public class GameLogic {
         ballCount = Configuration.BALL_COUNT_INITIAL;
         score = 0;
         paddle = new Paddle(this, Configuration.FIELD_X_SIZE / 2, Configuration.PADDLE_Y_POSITION,
-                Configuration.PADDLE_X_SIZE, Configuration.PADDLE_Y_SIZE, BLUE);
+                Configuration.PADDLE_X_SIZE, Configuration.PADDLE_Y_SIZE, Color.BLUE);
         ball = new Ball(this, Configuration.FIELD_X_SIZE, Configuration.PADDLE_Y_POSITION - 20,
                 Configuration.BALL_X_SIZE, Configuration.BALL_Y_SIZE, WHITE);
         bricks = new LinkedList<>();
